bb46a595f9fa13cb586c69df34263ad63fc7ae0f
Jeremy Chandler <goldenmeso@gmail.com>
2021-06-23 08:53:33 -0500

    refactor

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBJg2I8xb16QiLu4S-twXm5o1L3cWf2oVQ",
+    authDomain: "timecard-95463.firebaseapp.com",
+    projectId: "timecard-95463",
+    storageBucket: "timecard-95463.appspot.com",
+    messagingSenderId: "560667499267",
+    appId: "1:560667499267:web:af89e3449c4a51ecca2736",
+    measurementId: "G-1FZ61815QR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
