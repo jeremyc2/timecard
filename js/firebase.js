d50f9ae9c75e69717eed9468a6f9ae9c6d393f26
Jeremy Chandler <goldenmeso@gmail.com>
2021-07-26 09:07:07 -0500

    add auth library

--- js/firebase.js
+++ js/firebase.js
@@ -23,10 +23,8 @@
   ui.start('#firebaseui-auth-container', {
     signInOptions: [
       // List of OAuth providers supported.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     // Other config options...
+    signInSuccessUrl: '?page=Time Entry'
   });
